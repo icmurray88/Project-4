16a4bb1ea9e2e5fcde5faaeb66c6d50dc596f3ca
icmurray88 <63207840+icmurray88@users.noreply.github.com>
2020-04-06 14:00:43 -0400

    Add files via upload

---
+++ multiple.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+def printValues():
+    l = list()
+    for i in range(1,21):
+        l.append(i**2)
+    print(i,l)
+
+printValues()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
